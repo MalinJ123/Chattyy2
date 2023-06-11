7427d21dd179bc34c102f5ce01c6b2aba46b1eef
MalinJ123 <malin--jonsson@hotmail.com>
2023-06-11 16:43:19 +0200

    messages pratglada pelle Dm finns nu, ej funktionalitet

--- routes/DmMessages.jsx
+++ routes/DmMessages.jsx
@@ -1,9 +1,40 @@
 
+import React, { useState, useEffect } from "react";
+
 
 const DmMessages = ({ username }) => {
+  const [messages, setMessages] = useState([]);
+  
 	return (
-	  <p>Hej {username}</p>
+		<>
+			<div className="chat-area">
+				<section className="heading">
+					<p>Skriver med {username}</p>
+					<span className="chat-name"> </span>
+				</section>
+
+				<section className="history">
+					{messages.map((message, index) => (
+						<section key={index}>
+							<p>{message}</p>
+							<p>17:47</p>
+						</section>
+					))}
+				</section>
+				<section>
+          <form>
+					{/* <form onSubmit={handleMessageSubmit}> */}
+						<input
+							type="text"
+							name="message"
+							placeholder="Ditt meddelande..."
+						/>
+						<button type="submit">Skicka</button>
+					</form>
+				</section>
+			</div>
+		</>
 	);
- };
- 
- export default DmMessages;
+};
+
+export default DmMessages;
--- server.js
+++ server.js
@@ -2,60 +2,108 @@ import express from "express";
 import { join, dirname } from 'path'
 import { fileURLToPath } from 'url'
 import * as dotenv from 'dotenv'
+import { v4 as uuidv4 } from "uuid";
+import { Low } from 'lowdb';
+import { JSONFile } from 'lowdb/node';
+// import { getDb } from "../data/database.js";
 
-// Routers
+
+
+// // Routers
 import publicRouter from "./api/routes/public.js";
 import usersRouter from "./api/routes/users.js";
 import searchQuery from "./api/routes/search.js";
-import messagesRouter from "./api/routes/messages.js";
+// import messagesRouter from "./api/routes/messages.js";
 
 // Express saker
 dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 31140
 
-// The middlemen are looking at us
+app.use(express.json())
+
+const db = getDb(); 
+const whereWeAre = dirname(fileURLToPath(import.meta.url))
+const dist = join(whereWeAre, './dist')
+app.use( express.static(dist) )
+// -> Start
+
+function getDb() {
+	// Create search path to the database
+	const __dirname = dirname(fileURLToPath(import.meta.url))
+	const file = join(__dirname, 'db.json')
+	const adapter = new JSONFile(file)
+	const db = new Low(adapter, {})
+	// {} is default data
+	return db
+}
+
+
+// middleware and logger
+
+//logger and next function
+app.use((req, res, next) => {
+	console.log(`${req.method}  ${req.url} `, req.body)
+	// console.log('Auth header', req.headers.authorization)
+	next()
+})
+
 
 // CORS
 app.use((req, res, next) => {
 	res.header("Access-Control-Allow-Origin", "*")
 	next();
 })
-
 app.options('*', (req, res) => {
 	res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
 	res.header('Access-Control-Allow-Headers', 'Content-Type');
 	res.send();
 });
 
-app.use(express.json())
-
-// Logger
-app.use((req, res, next) => {
-	console.log(`${req.method}  ${req.url}`, req.body)
-	next()
-})
 
 
 
-// Routes //
-const whereWeAre = dirname(fileURLToPath(import.meta.url))
-const dist = join(whereWeAre, './dist')
-app.use( express.static(dist) )
-// -> Start
+// Messages
+app.post("/messages", async (req, res) => {
+	try {
+	  const { userId, message } = req.body;
+	  const newMessage = {
+		 id: uuidv4(),
+		 userId,
+		 message,
+	  };
+
+	  db.data.messages.push(newMessage);
+	  await db.write();
+ 
+	  console.log("Meddelandet har sparats i databasen.");
+ 
+	  res.sendStatus(200);
+	} catch (error) {
+	  console.log("Ett fel inträffade vid sparande av meddelandet i databasen.", error);
+	  res.sendStatus(500);
+	}
+ });
+
+app.get("/messages", async (req, res) => {
+	try {
+		const messages = db.data.messages; // Hämta alla meddelanden från databasen
+
+		console.log("Hämtade meddelanden från databasen:", messages);
+
+		res.send(messages);
+	} catch (error) {
+		console.log("Ett fel inträffade vid hämtning av meddelandena från databasen.", error);
+		res.sendStatus(500);
+	}
+});
 
 
-
-// -> CRUD products
+// -> public
 app.use('/api/public', publicRouter)
-
-// -> Messages 
-app.use('/api/messages', messagesRouter)
-
 // -> Search products
 app.use('/api/search', searchQuery)
-
-// -> CRUD users
+// -> users
 app.use('/api/users', usersRouter)
 
 app.get('*', (req, res) => {
