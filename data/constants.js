beeb3a70f6746e09832751e6f0cb94968886087e
MalinJ123 <malin--jonsson@hotmail.com>
2023-06-16 12:35:55 +0200

    url

--- data/constants.js
+++ data/constants.js
@@ -1,2 +1,3 @@
 // This needs to be changed after uploading api to render
-export const API_URL = 'http://localhost:31140/api/';
+export const API_URL = 'https://chatty-chat-app.onrender.com/api/';
+
--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': "http://localhost:31140/"
+      '/api': "https://chatty-chat-app.onrender.com"
     }
   }
 })
